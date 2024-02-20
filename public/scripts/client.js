ea9dfb6e7eb2b2aa4c3cd6371b5d1d05fff02b7a
NicholasTheisen <nicholas.theisen1997@gmail.com>
2024-02-20 09:48:39 +0000

    Finalizing up the Tweeter project

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,18 +4,26 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 const createTweetElement = function(tweet) {
   let $tweet = $(`
     <article class="tweet">
       <header>
-        <img src="${tweet.user.avatars}" alt="User Avatar">
-        <h2>${tweet.user.name}</h2>
-        <h4>${tweet.user.handle}</h4>
+        <div class="user-info">
+          <img src="${tweet.user.avatars}" alt="User Avatar">
+          <h2>${tweet.user.name}</h2>
+        </div>
+        <h4 class="user-handle">${tweet.user.handle}</h4>
       </header>
-      <p>${tweet.content.text}</p>
+      <p class="tweet-content">${escape(tweet.content.text)}</p>
       <footer>
-        <span>${timeago.format(tweet.created_at)}</span>
-        <div>
+        <span class="tweet-time">${timeago.format(tweet.created_at)}</span>
+        <div class="tweet-icons">
           <i class="fa fa-flag"></i>
           <i class="fa fa-retweet"></i>
           <i class="fa fa-heart"></i>
@@ -24,22 +32,21 @@ const createTweetElement = function(tweet) {
     </article>
   `);
   return $tweet;
-}
-
+};
 const renderTweets = function(tweets) {
   // Clear the tweets container before appending new tweets
   $('#tweets-container').empty();
-  for(let tweet of tweets) {
+  for(let tweet of tweets.reverse()) {
     const $tweet = createTweetElement(tweet);
     $('#tweets-container').append($tweet);
   }
-}
+};
 
 const loadTweets = function() {
   $.get('/tweets', function(data) {
     renderTweets(data);
   });
-}
+};
 
 $(document).ready(function() {
   loadTweets();
@@ -52,17 +59,20 @@ $(document).ready(function() {
 
     // Check if the tweet text is empty or too long
     if (tweetText === '') {
-      // Show an alert and return to prevent the form from being submitted
-      alert('Error: Tweet content cannot be empty');
+      // Show an error message and return to prevent the form from being submitted
+      $('#error-message').text('Error: Tweet content cannot be empty').slideDown();
       return;
     } else if (tweetText.length > 140) {
-      // Show an alert and return to prevent the form from being submitted
-      alert('Error: Tweet content cannot exceed 140 characters');
+      // Show an error message and return to prevent the form from being submitted
+      $('#error-message').text('Error: Tweet content cannot exceed 140 characters').slideDown();
       return;
     }
 
     let formData = $(this).serialize();
     $.post('/tweets', formData, function() {
+      // Clear the error message
+      $('#error-message').slideUp();
+      // Load the tweets again
       loadTweets();
     });
   });
